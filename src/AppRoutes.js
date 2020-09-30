69c942abaf12114346639d880c240d9fa38d9afa
Victor Hristov <victor.hristov@mentormate.com>
2020-09-30 21:37:08 +0300

    Add header

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,22 +1,43 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { Header } from './components/Header';
 import { Map } from './pages/Map';
 import { Login } from './pages/Login';
 import { Feed } from './pages/Feed';
 
 const AppRoutes = () => {
-	return (
-		<BrowserRouter>
-			<Switch>
-				<Route exact path="/" component={Map} />
-				<Route exact path="/feed" component={Feed} />
-				<Route exact path="/login" component={Login} />
-				<Redirect to="/">
-					<Route path="/" />
-				</Redirect>
-			</Switch>
-		</BrowserRouter>
-	);
+	const user = {
+		id: 1,
+		name: 'Victor',
+	};
+
+	if (user) {
+		return (
+			<div>
+				<Header />
+				<BrowserRouter>
+					<Switch>
+						<Route exact path="/" component={Map} />
+						<Route exact path="/feed" component={Feed} />
+						<Redirect to="/">
+							<Route path="/" />
+						</Redirect>
+					</Switch>
+				</BrowserRouter>
+			</div>
+		);
+	} else {
+		return (
+			<BrowserRouter>
+				<Switch>
+					<Route exact path="/login" component={Login} />
+					<Redirect to="/login">
+						<Route path="/login" />
+					</Redirect>
+				</Switch>
+			</BrowserRouter>
+		);
+	}
 };
 
 export { AppRoutes };
