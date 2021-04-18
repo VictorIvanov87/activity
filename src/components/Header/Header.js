42764bbb908619a039cdafa4b75b935bdf492cf8
Victor Hristov <victor.hristov@mentormate.com>
2021-04-18 11:48:28 +0300

    Handle profile logout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
 import logoSrc from '../../images/logo.png';
 import defaultProfile from '../../images/default-profile.png';
-import { EuiFieldText } from '@elastic/eui';
+import {
+	EuiFieldText,
+	EuiContextMenuPanel,
+	EuiContextMenuItem,
+	EuiPopover,
+} from '@elastic/eui';
 import {
 	HeaderContainer,
 	Logo,
@@ -9,20 +14,68 @@ import {
 	NavigationLink,
 	ProfileAvatar,
 } from './styled/Header.styled';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { NavLink, useHistory } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { useGoogleLogout } from 'react-google-login';
+import { setCurrentUserSuccess } from '../../store/slices/currentUser';
 
 const Header = (props) => {
+	const history = useHistory();
+	const dispatch = useDispatch();
 	const [search, setSearch] = useState('');
+	const [isPopoverOpen, setIsPopoverOpen] = useState(false);
 	const currentUser = useSelector((state) => state.currentUser);
+
+	const { signOut } = useGoogleLogout({
+		clientId:
+			'612839367197-tmqaq7mja08gfuunupfbnk3at9unmda8.apps.googleusercontent.com',
+		onLogoutSuccess: () => {
+			dispatch(setCurrentUserSuccess(null));
+			history.push('/login');
+		},
+		onFailure: () => {
+			console.log('logout fail');
+		},
+	});
 
 	const onChange = (e) => {
 		setSearch(e.target.value);
 	};
 
-	const handleImgError = e => {
-		e.target.src = defaultProfile;
-	  }
+	const handleImgError = (e) => (e.target.src = defaultProfile);
+
+	const onProfileClick = () =>
+		setIsPopoverOpen((isPopoverOpen) => !isPopoverOpen);
+
+	const closePopover = () => setIsPopoverOpen(false);
+
+	const goToProfilePage = () => {
+		setIsPopoverOpen(false);
+		history.push('/profile');
+	};
+
+	const logOut = () => {
+		setIsPopoverOpen(false);
+		signOut();
+	};
+
+	const profile = (
+		<ProfileAvatar
+			src={currentUser.data.imageUrl}
+			onError={handleImgError}
+			alt="profile picture"
+			onClick={onProfileClick}
+		/>
+	);
+
+	const profileDropdownItems = [
+		<EuiContextMenuItem key="profile" icon="user" onClick={goToProfilePage}>
+			Your Profile
+		</EuiContextMenuItem>,
+		<EuiContextMenuItem key="logout" icon="exit" onClick={logOut}>
+			Log Out
+		</EuiContextMenuItem>,
+	];
 
 	return (
 		<HeaderContainer>
@@ -40,9 +93,15 @@ const Header = (props) => {
 				<NavigationLink as={NavLink} to="/feed">
 					Feed
 				</NavigationLink>
-				<NavigationLink as={NavLink} to="/profile">
-					<ProfileAvatar src={currentUser.data.imageUrl} onError={handleImgError} alt="profile picture" />
-				</NavigationLink>
+				<div>
+					<EuiPopover
+						button={profile}
+						isOpen={isPopoverOpen}
+						closePopover={closePopover}
+					>
+						<EuiContextMenuPanel size="xs" items={profileDropdownItems} />
+					</EuiPopover>
+				</div>
 			</Navigation>
 		</HeaderContainer>
 	);
